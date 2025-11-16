8ae67f29ba71df2d7eca5762fdbbcdc862fd8912
theJRodrigues <gbrodrigues154+github@gmail.com>
2025-11-15 22:32:23 -0300

    criado arquivo de configurações do swagger

--- src/main/java/com/courses/CoursesApplication.java
+++ src/main/java/com/courses/CoursesApplication.java
@@ -1,18 +1,9 @@
 package com.courses;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(
-        info =
-        @Info(
-                title = "Cadastro de Cursos",
-                description = "API responsável pela gestão de cursos",
-                version = "1"))
-
 public class CoursesApplication {
 
     public static void main(String[] args) {
